d4bb9c335be8192b2dc7a9e89178e3c17a60618d
Josh Rands <jmrands@mymail.mines.edu>
2020-01-06 23:02:39 -0800

    another test pic

--- Deploy/camera.py
+++ Deploy/camera.py
@@ -6,11 +6,11 @@ cap = cv2.VideoCapture(0)
 ret, frame = cap.read()
 
 while True:
-    cv2.imshow('img1', frame)
-    if (cv2.waitKey(1) & 0xFF == ord('y')):
-        cv2.imwrite('frame.png',frame)
-        cv2.destroyAllWindows()
-        break
+#    cv2.imshow('img1', frame)
+#    if (cv2.waitKey(1) & 0xFF == ord('y')):
+    cv2.imwrite('frame.png',frame)
+    cv2.destroyAllWindows()
+    break
 
 cap.release()
 
