d84a9258de481251a16b181a1acfcb9e31b71898
Josh Rands <jmrands@mymail.mines.edu>
2020-01-06 20:54:17 -0800

    Got Encoders Working

--- Autonomy/CarStuff/Encoder_Test_V2.py
+++ Autonomy/CarStuff/Encoder_Test_V2.py
@@ -19,24 +19,24 @@ right_last_state = 0
 try:
     while True:
         left_sensor_state = GPIO.input(leftSensorPin)
-        if left_sensor_state and not left_last_state:
+        if left_sensor_state:
             print('left connected')
 
-        if not left_sensor_state and left_last_state:
+        if not left_sensor_state:
             print('left broken')
 
         left_last_state = left_sensor_state
 
         right_sensor_state = GPIO.input(rightSensorPin)
-        if right_sensor_state and not right_last_state:
+        if right_sensor_state:
             print('right connected')
 
-        if not right_sensor_state and right_last_state:
+        if not right_sensor_state:
             print('right broken')
 
         right_last_state = right_sensor_state
 
-        delay(0.010)
+        time.sleep(1)
 
 finally:
     GPIO.cleanup()
--- Autonomy/CarStuff/Interrupt_Test.py
+++ Autonomy/CarStuff/Interrupt_Test.py
@@ -12,18 +12,20 @@ rightCount = 0
 GPIO.setup(leftSensorPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(rightSensorPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def leftIterate():
-    leftCount += 1
+def leftIterate(channel):
+    global leftCount
+    leftCount = leftCount + 1
     print("Left Count: ", leftCount, "\n")
 
-def rightIterate():
-    rightCount+= 1
+def rightIterate(channel):
+    global rightCount
+    rightCount = rightCount + 1
     print("Right Count: ", rightCount, "\n")
 
-GPIO.add_event_detect(leftSensorPin, GPIO.FALLING, leftIt = leftIterate(), bouncetime = 300)
-GPIO.add_event_detect(rightSensorPin, GPIO.FALLING, rightIt = rightIterate(), bouncetime = 300)
+GPIO.add_event_detect(leftSensorPin, GPIO.FALLING, callback = leftIterate, bouncetime = 300)
+GPIO.add_event_detect(rightSensorPin, GPIO.FALLING, callback = rightIterate, bouncetime = 300)
 
-try:
+while True:
     print("Waiting for encoder moves..")
-    time.sleep(2)
+    time.sleep(20)
 
