6ff1b295043c631214379402a180bf9d9d4b2dc7
Josh Rands <jmrands@mymail.mines.edu>
2020-01-10 23:52:24 -0800

    Reading all input files

--- Deploy/MachineLearning/heart-attack-ml.py
+++ Deploy/MachineLearning/heart-attack-ml.py
@@ -6,6 +6,9 @@ from tensorflow.keras.optimizers import RMSprop
 import cv2
 import time
 import numpy as np
+import random
+
+train_data = []
 
 def getFlattenArray(img):
     # flatten image 
@@ -14,7 +17,7 @@ def getFlattenArray(img):
 
     return out_arr
 
-def getFramesFromVideo(file_name):
+def getFramesFromVideo(file_name, attack):
     print("Getting frames from " + file_name)
     cap = cv2.VideoCapture(file_name)
 
@@ -24,17 +27,31 @@ def getFramesFromVideo(file_name):
 #        cv2.imshow(image)
 #        print(image[0])
 #        print(getFlattenArray(image))
-        img_data = getFlattenArray(image)
-        for data in img_data:
-            print(data)
-        
+        flat_img = getFlattenArray(image)
+#        for data in img_data:
+#            print(data)
+
+        train_data.append([flat_img, attack])
+
         #cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file      
         success,image = cap.read()
-        print('Read a new frame: ', success)
+#        print('Read a new frame: ', success)
         count += 1
 
+    print("Read " + str(count) + " frames")
 
-getFramesFromVideo("./attack/parker1.mp4")
+getFramesFromVideo("./attack/parker1.mp4", 1)
+getFramesFromVideo("./attack/parker2.mp4", 1)
+getFramesFromVideo("./attack/parker3.mp4", 1)
+getFramesFromVideo("./attack/parker4.mp4", 1)
+getFramesFromVideo("./gooch/parker1.mp4", 0)
+getFramesFromVideo("./attack/parker2.mp4", 0)
+getFramesFromVideo("./attack/parker3.mp4", 0)
+getFramesFromVideo("./attack/parker4.mp4", 0)
+
+
+
+#print(train_data)
 #    print(in_arr)
 #    print("Output:")
 #    print(out_arr)
